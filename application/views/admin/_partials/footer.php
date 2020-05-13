b4ff5fb3780d81d88b215c8a14dd0041d0e7a498
unknown <iqbal.hadad06@gmail.com>
2020-05-13 16:26:03 +0700

    dashboard admin

--- application/views/admin/_partials/footer.php
+++ application/views/admin/_partials/footer.php
@@ -1,8 +1,8 @@
 <!-- /.content-wrapper -->
 <footer class="main-footer">
-      <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
-      All rights reserved.
-      <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.0.4
-      </div>
-    </footer>
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 1.0
+    </div>
+    <strong>Copyright &copy; 2020-2021 <a href="https://adminlte.io">Kelompok 2</a>.</strong> All rights
+    reserved.
+  </footer>
